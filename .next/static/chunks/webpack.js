3c79ce01d17fe2276e76af8ad8a22b5562c48c97
Michal Rowinski <121058727+Meefio@users.noreply.github.com>
2024-11-02 20:06:28 +0100

    some vercel fixes

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "a3d5761ea45cf7aa"; }
+/******/ 		__webpack_require__.h = function() { return "b869fd135e49830b"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
