bd39944bebb3487614bfa7cde15d79b11da1a107
Michal Rowinski <121058727+Meefio@users.noreply.github.com>
2024-11-05 22:32:27 +0100

    favicon updates...

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -41,6 +41,14 @@ export const metadata: Metadata = {
     description: 'Tworzymy szybkie i nowoczesne strony internetowe wykorzystując Next.js',
     images: ['/images/og-image.png'],
   },
+  icons: {
+    icon: [
+      { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+      { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+    ],
+    shortcut: '/favicon.ico',
+    apple: '/apple-icon.png',
+  },
 };
 
 export default function RootLayout({
