e4dd67b4ecae527bbc8358cea0efce0e5fd3820f
Michal Rowinski <121058727+Meefio@users.noreply.github.com>
2024-11-11 11:00:22 +0100

    sitemap updae

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -11,12 +11,7 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: 'daily' as const,
       priority: 1,
     },
-    {
-      url: `${baseUrl}/kontakt`,
-      lastModified: new Date(),
-      changeFrequency: 'weekly' as const,
-      priority: 0.8,
-    },
+
   ]
 
   // Strony sekcji
@@ -44,6 +39,12 @@ export default function sitemap(): MetadataRoute.Sitemap {
       lastModified: new Date(),
       changeFrequency: 'weekly' as const,
       priority: 0.9,
+    },
+    {
+      url: `${baseUrl}/kontakt`,
+      lastModified: new Date(),
+      changeFrequency: 'weekly' as const,
+      priority: 0.8,
     },
   ]
 
